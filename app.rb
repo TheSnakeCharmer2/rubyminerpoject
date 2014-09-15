3b69857bdf4132cb31fd33982e85f36a3cbd6ca3
dm47175 <devonrm98@gmailcom>
2014-09-15 12:22:47 -0400

    This is the project commit

---
+++ app.rb
@@ -0,0 +1,16 @@
+require 'sinatra'
+
+require './controllers/simple_routes.rb'
+
+
+set :public_folder, 'public'
+set :views, 'views'
+
+#Code
+#Canvas
+#Form
+#Input
+#Div
+#Ordered list, unordered list, list element
+#Audio
+#Anchor
